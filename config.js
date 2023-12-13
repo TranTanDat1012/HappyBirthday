0f9d71815bf6e3dbcb741beb4abf1af0f0e590c1
TranTanDat1012 <153602970+TranTanDat1012@users.noreply.github.com>
2023-12-13 17:55:49 +0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const nameGirl = 'For ...';
-const eventName = 'Happy Birthday';
-const titleCard = 'Chúc mừng giáng sinh';
-const contentCard = ' </br> Chúc mừng sinh nhật my friend';
-
-const giftImage = 'images1.png';
-const base64 = '';
-const giftImageBase64 = "data:image/png;base64, " + base64;
