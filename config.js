ddfa328f56fad9bd37ac897685ab2616190d4d36
TranTanDat1012 <153602970+TranTanDat1012@users.noreply.github.com>
2023-12-13 17:13:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,3 +2,7 @@ const nameGirl = 'For ...';
 const eventName = 'Merry Christmas';
 const titleCard = 'Chúc mừng giáng sinh';
 const contentCard = 'Giáng sinh vui vẻ </br> Merry Christmas';
+
+const giftImage = 'images1.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
